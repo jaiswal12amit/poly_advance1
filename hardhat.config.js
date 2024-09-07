652dbd97e5336e944a106f0957ef3cf9471b5fbf
jaiswal12amit <126448940+jaiswal12amit@users.noreply.github.com>
2024-09-07 12:09:04 +0530

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+module.exports = {
+  solidity: "0.8.18",
+};
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks:{
+    hardhat: {},
+    sepolia: {
+      url: 'https://rpc.sepolia.org',
+      accounts: [process.env.PRIVATE_KEY]
+    },   
+amoy: {
+      url: 'https://rpc-amoy.polygon.technology/',
+      accounts: [process.env.PRIVATE_KEY]
+},
+
+}
+};
